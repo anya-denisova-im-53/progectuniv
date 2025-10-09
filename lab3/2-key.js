b263816e1365513441a3d08a51ccc5d745c88650
anya denisova <denisova.anna@lll.kpi.ua>
2025-10-09 23:25:32 +0300

    third lab

---
+++ lab3/2-key.js
@@ -0,0 +1,15 @@
+
+
+const generatekey = (length, possible) => {
+  let result = '';
+  for (let i = 0; i < length; i++) {
+
+    const randomIndex = Math.floor(Math.random() * possible.length);
+    result += possible[randomIndex];
+  }
+  return result;
+};
+
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const key = generatekey(16, characters);
+console.log(key);
