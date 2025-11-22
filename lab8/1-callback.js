dfcc857932385e69188e2261654d296d835b92e6
anya denisova <denisova.anna@lll.kpi.ua>
2025-11-22 15:53:57 +0200

    last

---
+++ lab8/1-callback.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const iterate = (object, callback) => {
+  for (const key in object) {
+    if (Object.prototype.hasOwnProperty.call(object, key)) {
+      callback(key, object[key], object);
+    }
+  }
+};
+
+
